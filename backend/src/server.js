f9cf50b269e7c0c9490f9dd4134a122b2b192f05
Daniel Teixeira <dant25@gmail.com>
2019-08-12 14:05:02 -0300

    Banco agora esta na nuvem.

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
-// mongoose.connect('mongodb+srv://lukita:lukita@cluster0-gsrbh.mongodb.net/tindev?retryWrites=true&w=majority', {
-// useNewUrlParser: true
-// });
-
-mongoose.connect('mongodb://localhost:27017/Tinder', {
-    useNewUrlParser: true
+mongoose.connect('mongodb+srv://lukita:lukita@cluster0-gsrbh.mongodb.net/tindev?retryWrites=true&w=majority', {
+useNewUrlParser: true
 });
+
+// mongoose.connect('mongodb://localhost:27017/Tinder', {
+//     useNewUrlParser: true
+// });
 
 server.use(cors());
 server.use(express.json());
