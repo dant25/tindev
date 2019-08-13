7adecf07213f18a1a36035846895d2504ca925c4
Daniel Teixeira <dant25@gmail.com>
2019-08-13 11:50:41 -0300

    Alterando configurações de build.

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,12 @@ const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
-mongoose.connect('mongodb+srv://lukita:lukita@cluster0-gsrbh.mongodb.net/tindev?retryWrites=true&w=majority', {
-useNewUrlParser: true
+// mongoose.connect('mongodb+srv://lukita:lukita@cluster0-gsrbh.mongodb.net/tindev?retryWrites=true&w=majority', {
+// useNewUrlParser: true
+// });
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true
 });
 
 // mongoose.connect('mongodb://localhost:27017/Tinder', {
@@ -17,4 +21,11 @@ server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(process.env.PORT || 3333);
+app.use("/", express.static(__dirname + "/../../frontend/dist"));
+
+const port = process.env.PORT || 3333;
+// server.listen(process.env.PORT || 3333);
+
+server.listen(port, function () {
+    console.log(`Servidor executando em ${port}`)
+});
